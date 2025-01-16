035ec9eb263239404f9f40542d60f03cd30d92ad
lis24zy <tangbaoxkl@gmail.com>
2025-01-16 00:19:35 +0000

    Update Readme.R

--- Readme.R
+++ Readme.R
@@ -1,2 +1,2 @@
 ## read me
-## Hello
+## Aim:based on the quarterly data of the UK from 1989 to 2024, this study analyzes the correlation between the unemployment rate and GDP growth rate, inflation rate and employment rate, and predicts the future by analyzing the past trend combined with these variables.
