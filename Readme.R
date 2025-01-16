b69a9aeed6a4470c9728ed08a3e6feb1cc456eef
lis24zy <tangbaoxkl@gmail.com>
2025-01-16 00:35:59 +0000

    Update Readme.R

--- Readme.R
+++ Readme.R
@@ -1,5 +1,6 @@
 ## read me
+This is the coursework for module INF6027. My registration number is 240174868.
 Aim:based on the quarterly data of the UK from 1989 to 2024, this study analyzes the correlation between the unemployment rate and GDP growth rate, inflation rate and employment rate, and predicts the future by analyzing the past trend combined with these variables.
-The 'source' file is the data used in this experiment, including unemp for the unemployment rate, EMP for the employment rate, GDP and inflation The file "do file" is the code used in this experiment, with detailed comments to help understand.
-If you want to copy, just download 'source' and 'do file', open the 'do file' file and follow the steps.
+The 'Source' file is the data used in this experiment, including unemp for the unemployment rate, EMP for the employment rate, GDP and inflation The file "do file" is the code used in this experiment, with detailed comments to help understand.
+If you want to copy, just download 'Source' and 'do file', open the 'do file' file and follow the steps.
 The files' forecast_results_emp 'and' forecast_results_full 'are the predicted results, which you can use to compare with your own results.
